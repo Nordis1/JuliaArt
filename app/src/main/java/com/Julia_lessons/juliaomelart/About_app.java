e036a5eafdf5b65b6f2820ac72a26d1b1e0f8366
Stas <nordis654@gmail.com>
2020-01-14 23:52:59 +0200

    добавил переход на google play

--- app/src/main/java/com/Julia_lessons/juliaomelart/About_app.java
+++ app/src/main/java/com/Julia_lessons/juliaomelart/About_app.java
@@ -3,6 +3,7 @@ package com.Julia_lessons.juliaomelart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,10 +25,8 @@ public class About_app extends AppCompatActivity implements View.OnClickListener
         Intent intent;
         switch (v.getId()){
             case R.id.btnshare:
-                intent = new Intent(Intent.ACTION_SEND);
-                intent.setType("text/plain");
-                //intent.setType("message/rfc822/text/plain");
-                startActivity(Intent.createChooser(intent,"Через что поделиться?"));
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.Julia_lessons.juliaomelart"));
+                startActivity(intent);
                 break;
         }
     }
